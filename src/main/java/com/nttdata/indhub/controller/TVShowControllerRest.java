1dc81ba4738ff6c77cd1e775591ec2b70bcc69ad
Eric Faya Esteban <efayaest@emeal.nttdata.com>
2025-05-28 13:36:35 +0200

    New modified/renamed/added files

--- src/main/java/com/nttdata/indhub/controller/TVShowControllerRest.java
+++ src/main/java/com/nttdata/indhub/controller/TVShowControllerRest.java
@@ -13,17 +13,17 @@ public interface TVShowControllerRest {
 
   NetflixResponse<PostTVShowRest> getTVShowById(Long id) throws NetflixException;
 
-  NetflixResponse<PostTVShowRest> methodToCreateTVShow(PostTVShowRest tvShow) throws NetflixException;
+  NetflixResponse<PostTVShowRest> ceateTVShow(PostTVShowRest tvShow) throws NetflixException;
 
-  NetflixResponse<PostTVShowRest> methodToUpdateTVShow(PostTVShowRest tvShow) throws NetflixException;
+  NetflixResponse<PostTVShowRest> UpdateTVShow(PostTVShowRest tvShow) throws NetflixException;
 
   NetflixResponse<Object> deleteTVShow(Long id) throws NetflixException;
 
-  NetflixResponse<TVShowRest> methodToAddSeasonToTVShow(Long seasonId, Long tvShowId) throws NetflixException;
+  NetflixResponse<TVShowRest> addSeasonToTVShow(Long seasonId, Long tvShowId) throws NetflixException;
 
   NetflixResponse<TVShowRest> deleteSeasonOfTVShow(Long seasonId, Long tvShowId) throws NetflixException;
 
-  NetflixResponse<TVShowRest> methodToAddCategoryToTVShow(Long categoryId, Long tvShowId) throws NetflixException;
+  NetflixResponse<TVShowRest> ddCategoryToTVShow(Long categoryId, Long tvShowId) throws NetflixException;
 
-  NetflixResponse<TVShowRest> methodToDeleteCategoryOfTVShow(Long categoryId, Long tvShowId) throws NetflixException;
+  NetflixResponse<TVShowRest> DeleteCategoryOfTVShow(Long categoryId, Long tvShowId) throws NetflixException;
 }
