9bb258bb14120e25fc1252658eb41bef177766a8
Eric Faya Esteban <efayaest@emeal.nttdata.com>
2025-06-03 17:27:29 +0200

    New modified/renamed/added files

--- src/main/java/com/nttdata/indhub/controller/impl/ActorControllerRestImpl.java
+++ src/main/java/com/nttdata/indhub/controller/impl/ActorControllerRestImpl.java
@@ -32,30 +32,6 @@ public class ActorControllerRestImpl implements ActorControllerRest {
 
     @Override
     @ResponseStatus(HttpStatus.OK)
-    @GetMapping(value = RestConstantsUtils.RESOURCE_ACTORS, produces = MediaType.APPLICATION_JSON_VALUE)
-    @Operation(summary = "getAllActors", description = "Get all Actors paginated")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200"),
-            @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content),
-            @ApiResponse(responseCode = "403", description = "Forbidden", content = @Content)
-    })
-    public NetflixResponse<D4iPageRest<PostActorRest>> getAllActors(
-            @RequestParam(defaultValue = CommonConstantsUtils.ZERO) final int page,
-            @RequestParam(defaultValue = CommonConstantsUtils.TWENTY) final int size,
-            @Parameter(hidden = true) final Pageable pageable)
-            throws NetflixException {
-        final Page<PostActorRest> postActorRestList = actorService.getAllActors(pageable);
-        return new NetflixResponse<>(HttpStatus.OK.toString(),
-                String.valueOf(HttpStatus.OK.value()),
-                CommonConstantsUtils.OK,
-                new D4iPageRest<>(postActorRestList.getContent().toArray(PostActorRest[]::new),
-                        new D4iPaginationInfo(postActorRestList.getNumber(),
-                                pageable.getPageSize(),
-                                postActorRestList.getTotalPages())));
-    }
-
-    @Override
-    @ResponseStatus(HttpStatus.OK)
     @GetMapping(value = RestConstantsUtils.RESOURCE_ACTORS + RestConstantsUtils.RESOURCE_ACTOR_ID, produces = MediaType.APPLICATION_JSON_VALUE)
     @Operation(summary = "getActorById", description = "Get Actors by id")
     @ApiResponses(value = {
@@ -68,6 +44,22 @@ public class ActorControllerRestImpl implements ActorControllerRest {
         return new NetflixResponse<>(HttpStatus.OK.toString(),
                 String.valueOf(HttpStatus.OK.value()),
                 CommonConstantsUtils.OK, postActorRest);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping(value = RestConstantsUtils.RESOURCE_ACTORS_LARGE + RestConstantsUtils.RESOURCE_ACTOR_ID, produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "getActorInfoById", description = "Get Actors TVShows and Chapters by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content),
+            @ApiResponse(responseCode = "403", description = "Forbidden", content = @Content)
+    })
+    @Override
+    public NetflixResponse<ActorRest> getActorTVShowsChapters(final Long id) throws NetflixException {
+        final ActorRest actorRest = actorService.getActorTVShowsChapters(id);
+        return new NetflixResponse<>(HttpStatus.OK.toString(),
+                String.valueOf(HttpStatus.OK.value()),
+                CommonConstantsUtils.OK, actorRest);
     }
 
     @Override
@@ -119,20 +111,29 @@ public class ActorControllerRestImpl implements ActorControllerRest {
                 CommonConstantsUtils.OK);
     }
 
-
+    @Override
     @ResponseStatus(HttpStatus.OK)
-    @GetMapping(value = RestConstantsUtils.RESOURCE_ACTORS_LARGE + RestConstantsUtils.RESOURCE_ACTOR_ID, produces = MediaType.APPLICATION_JSON_VALUE)
-    @Operation(summary = "getActorInfoById", description = "Get Actors TVShows and Chapters by id")
+    @GetMapping(value = RestConstantsUtils.RESOURCE_ACTORS, produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "getAllActors", description = "Get all Actors paginated")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200"),
             @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content),
             @ApiResponse(responseCode = "403", description = "Forbidden", content = @Content)
     })
-    @Override
-    public NetflixResponse<ActorRest> getActorTVShowsChapters(final Long id) throws NetflixException {
-        final ActorRest actorRest = actorService.getActorTVShowsChapters(id);
+    public NetflixResponse<D4iPageRest<PostActorRest>> getAllActors(
+            @RequestParam(defaultValue = CommonConstantsUtils.ZERO) final int page,
+            @RequestParam(defaultValue = CommonConstantsUtils.TWENTY) final int size,
+            @Parameter(hidden = true) final Pageable pageable)
+            throws NetflixException {
+        final Page<PostActorRest> postActorRestList = actorService.getAllActors(pageable);
         return new NetflixResponse<>(HttpStatus.OK.toString(),
                 String.valueOf(HttpStatus.OK.value()),
-                CommonConstantsUtils.OK, actorRest);
+                CommonConstantsUtils.OK,
+                new D4iPageRest<>(postActorRestList.getContent().toArray(PostActorRest[]::new),
+                        new D4iPaginationInfo(postActorRestList.getNumber(),
+                                pageable.getPageSize(),
+                                postActorRestList.getTotalPages())));
     }
+
+
 }
